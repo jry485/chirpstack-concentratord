e159724a679981a514d9915f03d07bf9877e99f2
jry485 <jry_43@126.com>
2023-05-08 11:24:16 +0800

    CN490

--- chirpstack-concentratord-sx1302/src/config/vendor/waveshare/sx1302_lorawan_gateway_hat_cn490.rs
+++ chirpstack-concentratord-sx1302/src/config/vendor/waveshare/sx1302_lorawan_gateway_hat_cn490.rs
@@ -1,8 +1,9 @@
 use libloragw_sx1302::hal;
 
 use super::super::super::super::config;
-use super::super::{ComType, Configuration,Gps, RadioConfig};
+use super::super::{ComType, Configuration, Gps, RadioConfig};
 
+// source: https://github.com/Lora-net/sx1302_hal/blob/master/packet_forwarder/global_conf.json.sx1250.CN490
 pub fn new(conf: &config::Configuration) -> Configuration {
     Configuration {
         radio_count: 2,
